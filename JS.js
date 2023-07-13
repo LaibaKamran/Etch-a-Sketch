c310da6bf568036dbdc7a530d36710993810143c
Laiba Kamran <laiba.kamran1010@gmail.com>
2023-07-13 18:50:16 +0500

    add reset grid button

--- JS.js
+++ JS.js
@@ -1,4 +1,48 @@
 const container = document.getElementById('container');
+const resetButton = document.getElementById('resetButton');
+
+// Function to remove the existing grid
+function removeGrid() {
+  while (container.firstChild) {
+    container.firstChild.remove();
+  }
+}
+
+// Function to prompt the user for the number of squares per side
+function promptUser() {
+  const squaresPerSide = prompt('Enter the number of squares per side (maximum 100):');
+  const gridSize = parseInt(squaresPerSide);
+
+  // Validate the user input
+  if (gridSize && gridSize > 0 && gridSize <= 100) {
+    removeGrid();
+    generateGrid(gridSize);
+  } else {
+    alert('Invalid input! Please enter a number between 1 and 100.');
+  }
+}
+
+// Function to generate the new grid
+function generateGrid(gridSize) {
+  const squareSize = 100 / gridSize;
+
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      const square = document.createElement('div');
+      square.classList.add('square');
+      square.style.width = `${squareSize}%`;
+      square.style.paddingBottom = `${squareSize}%`;
+      container.appendChild(square);
+    }
+  }
+}
+
+// Add event listener to the reset button
+resetButton.addEventListener('click', promptUser);
+
+// Generate the initial grid
+generateGrid(16);
+
 
 // container.addEventListener('mouseover', (event) => {
 //     event.target.style.backgroundColor = 'black';
@@ -33,11 +77,11 @@ container.addEventListener('mouseover', (event) => {
 //     }
 //   });
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        container.appendChild(square);
-    }
-}
+// for (let i = 0; i < 16; i++) {
+//     for (let j = 0; j < 16; j++) {
+//         const square = document.createElement('div');
+//         square.classList.add('square');
+//         container.appendChild(square);
+//     }
+// }
 
